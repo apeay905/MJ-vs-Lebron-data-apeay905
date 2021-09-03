05e3b44aec0d456bd01e80747602ed7c9ad2e9cc
apeay905 <apeay905@gmail.com>
2021-09-02 20:28:48 -0500

    work

--- data/apeay.sql
+++ data/apeay.sql
@@ -1,7 +1,8 @@
-Select yearID
+--1.
+Select MAX(yearID), MIN(yearID)
 From teams;
 
-
+--2.
 Select 
 	nameFirst, nameLast, height, teamID, G_all
 From people AS p
@@ -10,7 +11,7 @@ ON p.playerID = a.playerID
 ORDER BY height ASC;
 
 
-
+--3.
 Select *
 From collegeplaying
 Where schoolid = 'vanderbilt';
@@ -29,5 +30,30 @@ ON p.playerID = s.playerID
 Where s2.schoolname = 'Vanderbilt University'
 GROUP BY p.nameFirst, p.nameLast, s2.schoolname
 ORDER BY s1 DESC;
+
+
+--4.
+Select Pos
+From Fielding;
+
+Select PO, 
+	CASE WHEN Pos = 'OF' THEN 'Outfield'
+		 WHEN Pos = '1B' AND Pos = '2B' AND Pos = 'SS' AND Pos = '3B' THEN 'Infield'
+		 ELSE 'Battery' END AS Position
+FROM Fielding
+Where yearID = '2016'
+Group BY Pos, PO;
+--Not working
+
+
+--5.
+Select CAST(AVG(SO) AS DECIMAL(10,2)), CAST(AVG(HR) AS DECIMAL(10,2)), (yearID)/10*10 AS decade
+From Batting
+GROUP BY yearID
+ORDER BY yearID DESC;
+
+
+--6.
+
 
 
